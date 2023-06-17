1ef9b41f4ad907cd5802d9695cf0ceec36f51fad
piyanshu <piyanshusinha.2807@gmail.com>
2023-06-17 23:45:31 +0530

    added files

---
+++ arrays/hard/Pascal's Triangle/var1.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r)
+{
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+void pascalTriangle(int n)
+{
+    // printing the entire row n:
+    for (int c = 1; c <= n; c++)
+    {
+        cout << nCr(n - 1, c - 1) << " ";
+    }
+    cout << "n";
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
